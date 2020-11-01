fb24138831aa0ebdf0780f872c46569c2924dd3c
Radiscipio <robertdiscipio@gmail.com>
2020-11-01 17:29:27 -0500

    updateStage function

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,8 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { createStage } from '../gameHelpers'
 
-export const useStage = () => {
-  const [stage, useStage] = useState(createStage())
+export const useStage = (player, resetPlayer) => {
+  const [stage, useStage] = useState(createStage());
+
+  useEffect(() => {
+
+    const updateStage = prevStage => {
+      // Clear the Stage
+      const newStage = prevStage.map(row => 
+        row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
+      );
+
+      // Make Tetromino
+        player.tetromino.forEach((row, y) => {
+          row.forEach((value, x) => {
+            
+          })
+        })
+
+    };
+
+    setStage(prev => updateStage(prev))
+  }, [])
 
   return [stage, useStage]
 }
