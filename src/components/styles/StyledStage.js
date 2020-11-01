52444d948fe0663e7f9cadc7c99348db364b9959
Radiscipio <robertdiscipio@gmail.com>
2020-11-01 01:01:38 -0400

    StyledStage component in Stage.js

--- src/components/styles/StyledStage.js
+++ src/components/styles/StyledStage.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const StyledStage = style.div`
+export const StyledStage = styled.div`
   display: grid;
   grid-template-rows: repeat(
     ${props => props.height}, 
