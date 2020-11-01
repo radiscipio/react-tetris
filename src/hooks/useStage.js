7f28bcb67879d30ce91360006f9ce196201b7d18
Radiscipio <robertdiscipio@gmail.com>
2020-11-01 18:13:11 -0500

    Collison check in useStage

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -24,11 +24,16 @@ export const useStage = (player, resetPlayer) => {
         })
       })
 
+      // Collision check
+      if(player.collided) {
+        resetPlayer();
+      }
+
       return newStage;
     };
 
     setStage(prev => updateStage(prev))
-  }, [player])
+  }, [player, resetPlayer])
 
   return [stage, setStage]
 }
