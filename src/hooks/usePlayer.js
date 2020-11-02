5fc49de26b280a895720e4622c22a9f046cf3a3c
Radiscipio <robertdiscipio@gmail.com>
2020-11-02 13:35:22 -0500

    Function to stop rotation from goign through walls

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,6 +1,6 @@
 import { useState, useCallback } from 'react'
 import { TETROMINOS, randomTetrominos } from '../tetrominos'
-import { STAGE_WIDTH } from '../gameHelpers'
+import { checkCollision, STAGE_WIDTH } from '../gameHelpers'
 
 export const usePlayer = () => {
   const [player, setPlayer] = useState({
@@ -17,7 +17,7 @@ export const usePlayer = () => {
     );
 
     // Reverse each row to get a rotated Matrix 
-    if (dir > 0) return rotatedTetro.map(row => row.reeverse());
+    if (dir > 0) return rotatedTetro.map(row => row.reverse());
     return rotatedTetro.reverse()
   };
 
@@ -25,6 +25,19 @@ const playerRotate = (stage, dir) => {
   const clonedPlayer = JSON.parse(JSON.stringify(player));
 
   clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
+
+  const pos = player.pos.x;
+  let offset = 1;
+  while(checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
+    clonedPlayer.pos.x += offset;
+    offset = -(offset + (offset > 0 ? 1 : -1));
+    if(offset > clonedPlayer.tetromino[0].length) {
+      rotate(clonedPlayer.tetromino, -dir);
+      clonedPlayer.pos.x = pos;
+      return;
+    }
+  }
+
   setPlayer(clonedPlayer);
 };
 
